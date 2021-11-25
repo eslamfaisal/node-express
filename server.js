9720decac10c5d908304583e095764a13672b4b3
eslamfaisal <eslam.faisal.ef@gmail.com>
2021-11-25 16:43:52 +0200

    server

--- server.js
+++ server.js
@@ -3,3 +3,34 @@ const express = require('express');
 
 // Start up an instance of app
 const app = express();
+/* Dependencies */
+const bodyParser = require('body-parser')
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+app.use(express.static('website'));
+
+// Load HTTP module
+const http = require("http");
+
+const hostname = "127.0.0.1";
+const port = 8000;
+
+// Create HTTP server
+const server = http.createServer(function(req, res) {
+
+    // Set the response HTTP header with HTTP status and Content type
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+
+    // Send the response body "Hello World"
+    res.end('Hello World\n');
+});
+
+// Prints a log once the server starts listening
+server.listen(port, hostname, function() {
+    console.log(`Server running at http://${hostname}:${port}/`);
+})
